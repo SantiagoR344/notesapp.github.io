262ea83f4cda59de09d29510e11b46e98bfb3d4f
Santiago Ramos <114268836+SantiagoR344@users.noreply.github.com>
2024-09-13 18:57:53 -0500

    Update server.js

--- Back-end/server.js
+++ Back-end/server.js
@@ -28,29 +28,5 @@ app.listen(PORT, () => {
 
 
 
-// // backend/server.js
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const authRoutes = require('./routes/auth');
-// const cors = require('cors');
 
-// const app = express();
-
-// // Middleware
-// app.use(cors());
-// app.use(express.json());  // Para parsear JSON
-
-// // Conectar a MongoDB
-// mongoose.connect('mongodb+srv://AFRAMOS12:Sapo30doblep@cluster0.oax05.mongodb.net/?retryWrites=true&w=majority')
-//   .then(() => console.log('Conectado a MongoDB'))
-//   .catch((err) => console.log('Error conectando a MongoDB', err));
-
-// // Rutas
-// app.use('/api/auth', authRoutes);
-
-// // Levantar el servidor
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`Servidor corriendo en el puerto ${PORT}`);
-// });
 
